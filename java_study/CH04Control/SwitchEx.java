3970cd9928c2392bb64c7187e55c5dee19e6022f
pshingsl <w455ok@gmail.com>
2026-02-25 15:28:50 +0900

    Java 제어문

---
+++ java_study/CH04Control/SwitchEx.java
@@ -0,0 +1,18 @@
+package Control;
+
+public class SwitchEx {
+    public static void main(String[] args) {
+        int num = 10;
+
+        switch (num){
+            case 10:
+                System.out.println("해당 숫자: " + num);
+                break;
+            case 9:
+                System.out.println("해당 숫자: " + num);
+                break;
+            default:
+                System.out.println("해당 숫자가 아닙니다.");
+        }
+    }
+}
