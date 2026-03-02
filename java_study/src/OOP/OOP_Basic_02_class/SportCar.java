04ecd31ffc4e3d20bce695816a06da520c3c10cc
pshingsl <w455ok@gmail.com>
2026-03-02 23:23:55 +0900

    Java 객체지향 프로그래밍(OOP)

---
+++ java_study/src/OOP/OOP_Basic_02_class/SportCar.java
@@ -0,0 +1,8 @@
+package OOP.OOP_Basic_01; // 퍀키지 선언
+
+public class SportCar { // 클래스 선언
+    int tire;     // 0
+    String color; // null
+    String name;  // null
+    boolean start; // false;
+}
---
+++ java_study/src/OOP/OOP_Basic_02_class/SportCarEx.java
@@ -0,0 +1,8 @@
+package OOP.OOP_Basic_01;
+
+public class SportCarEx {
+    public static void main(String[] args) {
+        SportCar s1 = new SportCar(); // 참조타입 SportCar s1변수 선언 -> SportCar의 기능 사용 가능
+        
+    }
+}
